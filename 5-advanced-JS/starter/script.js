7be621635d209c55db147cfa3645fd05f90e2430
Kristy Bui <bui.kristy@gmail.com>
2019-07-29 13:59:30 -0700

    Add correct coding challenge to section 5

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -38,3 +38,123 @@ var jane = Object.create(personProto, {
     yearOfBirth: { value: 1969 },
     job: { value: "designer" },
 });
+
+// =============================================================== //
+// Coding Challenge 7
+// =============================================================== //
+
+// SIMPLE VERSION
+/*
+(function() {
+    var Question = function(question, answers, correct) {
+        this.question = question;
+        this.answers = answers;
+        this.correct = correct;
+    }
+    
+    Question.prototype.displayQuestion = function() {
+        console.log(this.question);
+        for(var i = 0; i < this.answers.length; i++) {
+            console.log(i + ": " + this.answers[i]);
+        }
+    };
+    
+    Question.prototype.checkAnswer = function(ans) {
+        if(ans === this.correct) {
+            console.log("Correct answer!!");
+        } else {
+            console.log("Wrong...");
+        }
+    }
+    
+    var arrQuestions = [];
+    arrQuestions.push(new Question("Who is the coolest character in League?",
+                        ["Lux", "Teemo", "Blitzcrank"],
+                        0
+        ));
+    arrQuestions.push(new Question("Which cat is the coolest?",
+                        ["Peach", "Leo", "Ninja", "Rengar"],
+                        0
+    ));
+    arrQuestions.push(new Question ("Which drink is the best?",
+                        ["Coke", "Pepsi", "Smoothies", "Bubble Tea"],
+                        3
+    ));
+    
+    var rand = Math.floor(Math.random() * arrQuestions.length);
+    arrQuestions[rand].displayQuestion();
+    var answer = parseInt(prompt("Please select the right answer"));
+    arrQuestions[rand].checkAnswer(answer);
+})();
+*/
+
+
+
+    var Question = function(question, answers, correct) {
+        this.question = question;
+        this.answers = answers;
+        this.correct = correct;
+    }
+    
+    Question.prototype.displayQuestion = function() {
+        console.log(this.question);
+        for(var i = 0; i < this.answers.length; i++) {
+            console.log(i + ": " + this.answers[i]);
+        }
+    };
+    
+    Question.prototype.checkAnswer = function(ans, callback) {
+        var score;
+
+        if(ans === this.correct) {
+            console.log("Correct answer!!");
+            score = callback(true);
+        } else {
+            console.log("Wrong...");
+            score = callback(false);
+        }
+
+        this.displayScore(score);
+    }
+
+    Question.prototype.displayScore = function(score) {
+        console.log("Your current score is : " + score);
+        console.log("-----------------------------------");
+    }
+    
+    var arrQuestions = [];
+    arrQuestions.push(new Question("Who is the coolest character in League?",
+                        ["Lux", "Teemo", "Blitzcrank"],
+                        0
+        ));
+    arrQuestions.push(new Question("Which cat is the coolest?",
+                        ["Peach", "Leo", "Ninja", "Rengar"],
+                        0
+    ));
+    arrQuestions.push(new Question ("Which drink is the best?",
+                        ["Coke", "Pepsi", "Smoothies", "Bubble Tea"],
+                        3
+    ));
+
+    function score() {
+        var score = 0;
+        return function(correct) {
+            if(correct) {
+                score++;
+            }
+            return score;
+        }
+    }
+
+    var keepScore = score();
+    function nextQuestion() {
+        var rand = Math.floor(Math.random() * arrQuestions.length);
+        arrQuestions[rand].displayQuestion();
+        var answer = prompt("Please select the right answer");
+        if(answer !== "exit") {
+            arrQuestions[rand].checkAnswer(parseInt(answer), keepScore);
+            nextQuestion();
+        }
+    }
+    
+    nextQuestion();
