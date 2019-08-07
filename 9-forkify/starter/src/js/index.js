0d92d92993552b352fd4f32dc3b23a1bb903cdf1
Kristy Bui <bui.kristy@gmail.com>
2019-08-07 16:34:27 -0700

    Finish adding search and page flip functionality

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,4 +1,5 @@
 import Search from "./models/Search";
+import Recipe from "./models/Recipe";
 import { elements, renderLoader, clearLoader } from "./views/base";
 import * as searchView from "./views/searchView";
 
@@ -10,9 +11,11 @@ import * as searchView from "./views/searchView";
  */
 const state = {};
 
+/* SEARCH CONTROLLER */
 const controlSearch = async () => {
     // 1. get query from view
-    const query = searchView.getInput(); 
+    //const query = searchView.getInput(); 
+    const query = "pizza";
 
     if(query) {
         // 2. new search object and add to state
@@ -23,12 +26,17 @@ const controlSearch = async () => {
         searchView.clearResults();
         renderLoader(elements.searchRes);
 
-        // 4. search for recipes
-        await state.search.getResults();
-
-        // 5. render results on UI
-        clearLoader();
-        searchView.renderResults(state.search.result);
+        try {
+            // 4. search for recipes
+            await state.search.getResults();
+    
+            // 5. render results on UI
+            clearLoader();
+            searchView.renderResults(state.search.result);
+        } catch {
+            alert('Error getting results');
+            clearLoader();
+        }
     }
 };
 
@@ -36,3 +44,51 @@ elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
     controlSearch();
 });
+
+// TESTING 
+window.addEventListener('load', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
+elements.searchResPages.addEventListener('click', e => {
+    const btn = e.target.closest('.btn-inline');
+    if(btn) {
+        const goToPage = parseInt(btn.dataset.goto, 10);
+        searchView.clearResults();
+        searchView.renderResults(state.search.result, goToPage);
+    }
+});
+
+/* RECIPE CONTROLLER */
+
+const controlRecipe = async () => {
+    // get id from url
+    const id = window.location.hash.replace("#", "");
+
+    if(id) { 
+        // 1. prepare UI for changes
+
+        // 2. create new recipe object
+        state.recipe = new Recipe(id);
+        window.r = state.recipe; // TESTING VARIABLE
+
+        try {
+            // 3. get recipe data
+            await state.recipe.getRecipe();
+
+            // 4. calculate servings and time
+            state.recipe.calcTime();
+            state.recipe.calcServings();
+
+            // 5. render recipe
+            console.log(state.recipe);
+        } catch {
+            alert('Error processing recipe');
+        }
+    }
+}
+
+//window.addEventListener('hashchange', controlRecipe);
+//window.addEventListener('load', controlRecipe);
+['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
--- 9-forkify/starter/src/js/models/Search.js
+++ 9-forkify/starter/src/js/models/Search.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import {key} from './../config';
 
 export default class Search {
     constructor (query) {
@@ -7,7 +8,6 @@ export default class Search {
 
     async getResults() {
         const url = "https://www.food2fork.com/api/search";
-        const key = '090889bd6c48f394d47f12858fab964c';
 
         try {
             const res = await axios(`${url}?key=${key}&q=${this.query}`);
